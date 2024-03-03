1a93f1b41bc4cec8d61d5aa82b72c54516c80dad
vasavi <155808521+vasavi666@users.noreply.github.com>
2024-03-03 13:01:53 +0530

    Create Day17project2.js

---
+++ Week3/Day17/Day17project2.js
@@ -0,0 +1,17 @@
+// define two numbers to add
+let input1 = 10;
+let input2 = 20;
+
+// add the numbers using the + operator
+let sum = input1 + input2;
+let sub = input1 - input2;
+let multi = input1 * input2;
+let div = input1 / input2;
+let modulus = input1 % input2;
+
+// display the result
+console.log("The sum of " + input1 + " and " + input2 + " is: " + sum);
+console.log("The subtraction of " + input1 + " and " + input2 + " is: " + sub);
+console.log("The multiplication of " + input1 + " and " + input2 + " is: " + multi);
+console.log("The division of " + input1 + " and " + input2 + " is: " + div);
+console.log("The modulus of " + input1 + " and " + input2 + " is: " + modulus);
