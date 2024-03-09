872e513131b356505e2f2ac8bb6784cbb7f2728c
vasavi <155808521+vasavi666@users.noreply.github.com>
2024-03-09 20:14:58 +0530

    Create Day27.js

---
+++ Week 4/Day27/Day27.js
@@ -0,0 +1,10 @@
+// Simple React component
+import React from "react";
+
+class Greeting extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}!</h1>;
+  }
+}
+
+export default Greeting;
