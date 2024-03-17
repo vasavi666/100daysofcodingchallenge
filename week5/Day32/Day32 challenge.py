2392e3b06fad4b20944be2d07cda11802c99c850
vasavi <155808521+vasavi666@users.noreply.github.com>
2024-03-17 22:48:48 +0530

    Create Day32 challenge.py

---
+++ week5/Day32/Day32 challenge.py
@@ -0,0 +1,21 @@
+ age = int(input("Please enter your age: "))
+ 
+if age < 18:
+    print("You are a minor.")
+elif 18 <= age <= 70:
+    print("You are an adult.")
+else:
+    print("You are a senior citizen.")
+
+
+# Ask the user to input a number
+ num=int(input("enter any number"))
+if num>=0:
+  if num==0:
+    print("zero")
+  else:
+    print("Positive Number!")
+else:
+    print("Negative Number!")
+ 
+ 
