08864de6876c483ad92fa2eaadd2726ef6480bfa
vasavi <155808521+vasavi666@users.noreply.github.com>
2024-03-17 22:33:43 +0530

    Create Day30 challenge.py

---
+++ week5/Day30/Day30 challenge.py
@@ -0,0 +1,11 @@
+ 
+age = 18
+height = 1.56
+first_name = "vasavi"
+ 
+message = "Hello, " + first_name + "!"
+print(message)
+ 
+age_square = age ** 2
+print("The square of your age is:", age_square)
+ 
